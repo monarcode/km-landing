e1f936f650d7c03f1ee6cd857c9aac8ab5b1a656
monarcode <ugbahisioma@gmail.com>
2020-10-06 19:47:00 +0100

    links

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,27 @@
-// window.addEventListener("load", function () {
-//     const pageLoader = document.querySelector(".loader-wrapper");
-//     const body = document.querySelector('body');
-//     pageLoader.className += " hide";
-//     body.classList.remove("no-scroll"); 
-// })
+$(document).ready(function () {
 
-$('body').scrollspy({
-    target: '#nav'
+    var device = navigator.userAgent;
+
+    console.log(device);
+
+    $('body').scrollspy({
+        target: '#nav'
+    })
+
+    $('#app-download').click(function () {
+
+        if (device.match(/Android|Windows/i)) {
+            console.log('Android');
+            window.open("https://play.google.com/store/apps/details?id=com.teqpace.kolomoni&hl=en");
+        } else if (device.match(/Mac|Safari|iPhone|iPad|AppleWebKit/i)) {
+            console.log('iOS')
+            window.open("https://apps.apple.com/ng/app/kolomoni/id1479338235");
+        }
+    })
+
+    AOS.init({
+        delay: 0,
+        duration: 1500,
+        easing: 'ease-in-out'
+    });
 })
-
-AOS.init({
-    delay: 0,
-    duration: 1500,
-    easing: 'ease-in-out'
-});
